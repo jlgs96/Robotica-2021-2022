a9a8e00813a3d6f4d914368491675ca0b3b0d4eb
jlgs96 <joselgs96@gmail.com>
2021-09-14 10:18:11 +0200

    Segundo commit, subiendo entero ContadorStd

---
+++ ContadorStd/ejemplo1.h
@@ -0,0 +1,30 @@
+#ifndef ejemplo1_H
+#define ejemplo1_H
+
+#include <QtGui>
+#include "ui_counterDlg.h"
+#include <chrono>
+#include "timer.h"
+
+
+class ejemplo1 : public QWidget, public Ui_Counter
+{
+Q_OBJECT
+    public:
+        ejemplo1();
+        virtual ~ejemplo1();
+    
+    public slots:
+	void doButton();
+        
+    private:
+        Timer mytimer, mytimerLong;
+        int cont = 0;
+		
+		// dos callbacks con diferente número de parámetros
+        void cuenta();
+		
+		int trick = 5;
+};
+
+#endif // ejemplo1_H
